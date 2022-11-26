8f9dab621f16498678e6cac296245b8a11664c33
Your Name <you@example.com>
2022-11-26 14:05:04 +0100

    tr

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
+from datetime import datetime
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+db = SQLAlchemy(app)
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    link = db.Column(db.String(100), nullable=False, default=datetime.now)
+    created_at = db.Column()
+    finished = db.Column()
+
+    def __repr__(self):
+        return f'<Project {self.title}>'
 
 @app.route("/")
 def hello_world():
